1a1b3a9df97a83df3c284df70d7e99ca2cfdb911
Dayron-z <zbastian.zapata@gmail.com>
2024-07-03 13:03:20 -0500

    Solid

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.ordenes.Order;
+import org.example.ordenes.Print;
+import org.example.ordenes.Save;
+
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        }
+    }
---
+++ src/main/java/org/example/ordenes/Save.java
@@ -0,0 +1,7 @@
+package org.example.ordenes;
+
+public class Save {
+    public void saveToDatabase(Order order){
+        System.out.println("La orden ha sido guardada con exito ");
+    }
+}
