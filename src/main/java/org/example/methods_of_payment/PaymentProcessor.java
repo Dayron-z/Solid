9b3b6c6b05d46dd1be90905163f7ade47e1e5010
Dayron-z <zbastian.zapata@gmail.com>
2024-07-03 13:04:07 -0500

    Cambio

--- src/main/java/org/example/methods_of_payment/PaymentProcessor.java
+++ src/main/java/org/example/methods_of_payment/PaymentProcessor.java
@@ -2,6 +2,7 @@ package org.example.methods_of_payment;
 
 /*Sí, en Java, es completamente válido y común usar una interfaz como parámetro de un método. Esto permite que el método acepte cualquier objeto que implemente esa interfaz, facilitando el uso de polimorfismo y promoviendo una mayor flexibilidad y extensibilidad en tu código.*/
 public class PaymentProcessor  {
+
     public void processPayment(PaymentMethod paymentMethod) {
         paymentMethod.procesPayment();
     }
